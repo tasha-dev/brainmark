0916409e6362e46f341810218000c53993ba1dc9
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2025-12-17 13:27:08 +0330

    initialized shad cn into the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { RootLayoutProps } from "@/type/component";
 import type { Metadata } from "next";
 import { JSX } from "react";
 import { Open_Sans } from "next/font/google";
+import "@/app/globals.css";
+import { cn } from "@/lib/util";
 
 // Defining metadata
 export const metadata: Metadata = {
@@ -23,7 +25,14 @@ export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
   // Returning JSX
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body
+        className={cn(
+          "min-h-dvh overflow-x-hidden overflow-y-auto",
+          OpenSansFont.className,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
